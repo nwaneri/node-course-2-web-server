ad1249a1f0b19327f502553af83c2fbd365799a6
Nwaneri Richard <youngrichie007@gmail.com>
2019-01-31 13:49:43 +0100

    This is the first version

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app =  express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+// app.use((req, res, next) => {
+//   var now = new Date().toString();
+//   res.render('maintainance.hbs');
+//
+//   next();
+// });
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs',{
+    header:"Find out about us",
+    body: "Fill the form below and ask us any question you would like to!!!",
+    currentYear: new Date().getFullYear()
+  })
+});
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(sentText) => {
+  return sentText.toUpperCase();
+})
+
+
+app.get('/', (req,res) => {
+  res.render('home.hbs',{
+    header:"Welcome to our Home Page",
+    body: "We offer a lot of things here like live results etc!!!",
+    currentYear: new Date().getFullYear()
+  })
+});
+
+
+app.get('/bad',(req,res) => {
+  res.send('Ogbeni this page does not exit at all');
+});
+
+app.listen(3000);
